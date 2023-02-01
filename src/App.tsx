d16e70e113e213e2e7785f403782ae35cf05de68
ivankupryianau <ivankuperstudy@gmail.com>
2023-02-01 22:32:39 +0100

    feat: make default design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technoliges";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className='header'>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPHRvtFUvNT9Rrpz2HE4gu05hPPg8m7DweCg&usqp=CAU"
+                    alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt=""/>
+                </div>
+                <div>
+                    av + desc
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New Post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 }
