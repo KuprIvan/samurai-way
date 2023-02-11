1ce1cd704360cf62dc2e1d2e5e86e4e4139c4423
ivankupryianau <ivankuperstudy@gmail.com>
2023-02-11 13:26:06 +0100

    week2 - refactor components code, new components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, {FC} from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
+
+
+type DialogItemType = {
+    id: string
+    name: string
+}
+
+type MessageItemType = {
+    title: string
+}
+const DialogItem: FC<DialogItemType> = (props) => {
+    let path = `/dialogs/${props.id}`
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    )
+}
+
+const MessageItem: FC<MessageItemType> = (props)  => {
+  return (
+      <div className={s.message}>
+          {props.title}
+      </div>
+  )
+}
+
+
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <DialogItem name='Ivan' id='1'/>
+                <DialogItem name='Nastya' id='2'/>
+                <DialogItem name='Denis' id='3'/>
+                <DialogItem name='Ihor' id='4'/>
+                <DialogItem name='Sveta' id='5'/>
+            </div>
+            <div className={s.messages}>
+                <MessageItem title='Hi' />
+                <MessageItem title='Yo' />
+                <MessageItem title='Bye' />
+            </div>
         </div>
     );
 };
